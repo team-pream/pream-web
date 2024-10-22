4ba7d98c82501b408a05b5da0b905106ddf71376
sieon <sieon.eve@gmail.com>
2024-10-22 21:23:33 +0900

    feat: 상세페이지 스타일 추가

---
+++ src/pages/detail/index.style.ts
@@ -0,0 +1,16 @@
+import { css } from '@emotion/react';
+import color from '@/style/colors';
+
+export const Info_wrapp = css`
+  display: flex;
+  padding: 3px 20px;
+`;
+
+export const list_title = css`
+  font-size: 12px;
+  color: ${color};
+`;
+
+export const list_content = css`
+  font-size: 12px;
+`;
