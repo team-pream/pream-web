791e0fe9698b6ca0cd6b820a4ba7e51f7fa494ca
sieon <sieon.eve@gmail.com>
2024-10-23 20:00:05 +0900

    feat: 상품상세 페이지 구현 및 캐러셀 구현

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ export default function App() {
           <Route path="/" element={<Main />} />
           <Route path="/auth/*" element={<Auth />} />
           <Route path="/products/:productId" element={<Detail />} />
-          <Route path="/detail" element={<Detail />} />
           <Route path="/address" element={<Address />} />
           <Route path="/login" element={<Login />} />
           <Route path="/onboarding" element={<OnBoarding />} />
