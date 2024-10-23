2267fa8d4174b6aa8d68068f268f011f3da35ca1
sieon <sieon.eve@gmail.com>
2024-10-23 20:00:05 +0900

    feat: 상품상세 페이지 구현 및 캐러셀 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import QueryProvider from '@/provider/query-provider';
 import GlobalStyle from '@/styles/global-style';
-import { Main, OnBoarding, Category, Login, Auth, Products, Detail, Address } from '@/pages';
+import { Main, OnBoarding, Category, Login, Auth,  Detail, Address } from '@/pages';
+import Purchase from './pages/purchase';
 
 export default function App() {
   return (
