5c1914665caecc2424ffda0ff56e1fc8be993adb
minju0423 <lmj042398@gmail.com>
2024-10-26 00:18:45 +0900

    feat: 상품 목록 페이지 UI 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import QueryProvider from '@/provider/query-provider';
 import GlobalStyle from '@/styles/global-style';
-import { Login, Main, OnBoarding, Category } from '@/pages';
+import { Login, Main, OnBoarding, Category, Products } from '@/pages';
 import KakaoCallback from './pages/auth/pages/kakao';
+// import { ROUTE_PATHS } from './constants/routes';
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
           <Route path="/auth/kakao" element={<KakaoCallback />} />
           <Route path="/onboarding" element={<OnBoarding />} />
           <Route path="/category" element={<Category />} />
+          <Route path="/products" element={<Products />} />
         </Routes>
       </BrowserRouter>
     </QueryProvider>
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,3 +3,4 @@ export { default as Login } from './auth';
 export { default as OnBoarding } from './onboarding';
 
 export { default as Category } from './category';
+export { default as Products } from './products';
