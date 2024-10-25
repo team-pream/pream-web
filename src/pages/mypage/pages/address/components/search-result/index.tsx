5a64e017850f42c0e0d989f9d1ca6e0292d1b1a3
sieon <sieon.eve@gmail.com>
2024-10-25 13:41:03 +0900

    feat: 버튼 생성 및 검색결과 요소

--- src/pages/mypage/pages/address/components/search-result/index.tsx
+++ src/pages/mypage/pages/address/components/search-result/index.tsx
@@ -3,10 +3,18 @@ import { searchResultWrapper, searchTitle, resultAddress, addressTag } from './i
 const SearchResult: React.FC = () => {
   return (
     <div css={searchResultWrapper}>
-      <div css={searchTitle}></div>
-      <div>
-        <div css={addressTag}></div>
-        <div css={resultAddress}></div>
+      <div css={searchTitle}>주소를 검색했습니다.</div>
+      <div css={{ display: 'flex' }}>
+        <div css={addressTag}>도로명</div>
+        <div css={resultAddress}>주소입니다. 주소요</div>
+      </div>
+      <div css={{ display: 'flex' }}>
+        <div css={addressTag}>도로명</div>
+        <div css={resultAddress}>주소입니다. 주소요</div>
+      </div>
+      <div css={{ display: 'flex' }}>
+        <div css={addressTag}>도로명</div>
+        <div css={resultAddress}>주소입니다. 주소요</div>
       </div>
     </div>
   );
