6743399e340b44364debfe3212cdb6f38929f507
sieon <sieon.eve@gmail.com>
2024-10-25 13:41:03 +0900

    feat: 버튼 생성 및 검색결과 요소

--- src/App.tsx
+++ src/App.tsx
@@ -12,11 +12,10 @@ export default function App() {
           <Route path="/" element={<Main />} />
           <Route path="/auth/*" element={<Auth />} />
           <Route path="/products/:productId" element={<Detail />} />
+          <Route path="/detail" element={<Detail />} />
           <Route path="/address" element={<Address />} />
           <Route path="/login" element={<Login />} />
           <Route path="/onboarding" element={<OnBoarding />} />
-          <Route path="/category" element={<Category />} />
-          <Route path="/products" element={<Products />} />
         </Routes>
       </BrowserRouter>
     </QueryProvider>
