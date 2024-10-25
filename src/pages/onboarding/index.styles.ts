1642d4d94555b7ca07b6a2c6d02387fa67652478
Kyungwon Jeon <96944509+jeonkyungwon@users.noreply.github.com>
2024-10-25 11:03:19 +0900

    [PREAM-15] 로그인 페이지 개발 (#6)
    * feat: 로그인 페이지 UI 기초 틀 구현
    
    * feat: 로그인 페이지 개발
    
    * fix: 코드리뷰 사항 수정
    
    * fix: api 주소 변수 명 변경

---
+++ src/pages/onboarding/index.styles.ts
@@ -0,0 +1,9 @@
+import { css } from '@emotion/react';
+
+export const Wrapper = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
