32a6746affd876a62095359384c991f2d8ebcc8a
Yoonjeong Ko <jejukyj@naver.com>
2024-10-25 16:08:41 +0900

    feat: 메인페이지 섹션별 상품 조회 UI 구현

--- src/pages/main/index.styles.ts
+++ src/pages/main/index.styles.ts
@@ -1,3 +1,4 @@
+import theme from '@/styles/theme';
 import { css } from '@emotion/react';
 
 export const wrapper = css`
@@ -32,4 +33,78 @@ export const categoryItem = css`
   gap: 7px;
   font-size: 10px;
   font-weight: 500;
+`;
+
+export const listWrapper = css`
+  margin: 40px 0 0 18px;
+`;
+
+export const listTitle = css`
+  font-size: 16px;
+  font-weight: 600;
+`;
+
+export const itemList = css`
+  margin-top: 15px;
+  display: flex;
+  gap: 9px;
+  margin-bottom: 30px;
+  overflow: auto;
+`;
+
+export const item = css`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const imageBox = css`
+  position: relative;
+`;
+
+export const sampleImage = css`
+  background-color: lightgray;
+  border-radius: 13px;
+  width: 120px;
+  height: 120px;
+`;
+
+export const opacityBox = css`
+  display: flex;
+  border-radius: 13px;
+  width: 120px;
+  height: 120px;
+  position: absolute;
+  z-index: 1;
+  background-color: ${theme.colors.grayOpacity200};
+`;
+
+export const heartIcon = css`
+  width: 18px;
+  height: 15px;
+  position: absolute;
+  right: 10px;
+  bottom: 10px;
+  z-index: 2;
+  cursor: pointer;
+  path {
+    fill: ${theme.colors.white};
+  }
+`;
+
+export const textBox = css`
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+  padding: 8px 0 0 7px;
+  cursor: pointer;
+`;
+
+export const itemTitle = css`
+  font-size: 12px;
+  font-weight: 500;
+`;
+
+export const itemPrice = css`
+  font-size: 16px;
+  font-weight: 600;
 `;
