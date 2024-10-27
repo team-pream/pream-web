c206e0336e90d9bf5d963e2f481d359cc16f0bed
sieon <sieon.eve@gmail.com>
2024-10-27 18:10:58 +0900

    fix: 모달창 생성 및 상세주소 입력 액션 수정

--- src/pages/mypage/pages/address/components/addAddress/index.style.ts
+++ src/pages/mypage/pages/address/components/addAddress/index.style.ts
@@ -32,9 +32,32 @@ export const addressTag = css`
 `;
 
 export const resultAddress = css`
-  width: 90%;
+  width: 100%;
   margin: 0 auto;
-  margin: 5px 0;
+  margin: 2px 0;
   cursor: pointer;
   display: flex;
+  font-size: 14px;
+`;
+
+export const inputStyle = (isFocused: boolean) => css`
+  margin: 0px auto;
+  display: flex;
+  border-bottom: 2px solid ${isFocused ? theme.colors.green200 : theme.colors.gray200};
+  transition: border-color 0.3s ease;
+  justify-content: flex-end;
+  :focus {
+    outline: none;
+  }
+  input {
+    margin: 10px;
+    font-size: 14px;
+    flex: 1;
+    border: none;
+    outline: none;
+  }
+  input::placeholder {
+    font-size: 12px;
+    color: ${theme.colors.gray300};
+  }
 `;
