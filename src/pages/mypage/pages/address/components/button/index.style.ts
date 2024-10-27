527e62bcb38c59e9e1b270f7567083226a3c75b9
sieon <sieon.eve@gmail.com>
2024-10-27 18:10:58 +0900

    fix: 모달창 생성 및 상세주소 입력 액션 수정

---
+++ src/pages/mypage/pages/address/components/button/index.style.ts
@@ -0,0 +1,21 @@
+import { css } from '@emotion/react';
+import theme from '@/styles/theme';
+
+export const ButtonStyle = css`
+  position: relative;
+  top: 240px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 100%;
+  padding: 16px 0;
+  background-color: ${theme.colors.green200};
+  color: white;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  font-size: 14px;
+  &:disabled {
+    background-color: #ccc;
+    cursor: not-allowed;
+  }
+`;
