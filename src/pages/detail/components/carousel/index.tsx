99aaabdbefe53c6431b0fc32c11d86c9b634e3db
sieon <sieon.eve@gmail.com>
2024-10-28 16:37:40 +0900

    feat: api 연동

--- src/pages/detail/components/carousel/index.tsx
+++ src/pages/detail/components/carousel/index.tsx
@@ -1,15 +1,12 @@
 import React, { useState } from 'react';
 import theme from '@/styles/theme';
 import { CarouselWrapper, CarouselImg, LeftBtn, RightBtn } from './index.style';
-// 이미지 파일을 import합니다.
-import img1 from '../../../../assets/images/img1.jpg';
-import img2 from '../../../../assets/images/img2.jpg';
-import img3 from '../../../../assets/images/img3.jpg';
-import img4 from '../../../../assets/images/img4.jpg';
 
-const images = [img1, img2, img3, img4];
+interface CarouselProps {
+  images: string[]; // 이미지 URL 배열을 props로 받음
+}
 
-const Carousel: React.FC = () => {
+const Carousel: React.FC<CarouselProps> = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0); // 현재 슬라이드 인덱스
 
   const handlePrev = () => {
@@ -28,8 +25,8 @@ const Carousel: React.FC = () => {
       <div
         css={CarouselImg}
         style={{
-          width: `${images.length * 390}px`,
-          transform: `translateX(${-currentIndex * 390}px)`,
+          width: `${images.length * 480}px`, // 최대 너비 1440px
+          transform: `translateX(${-currentIndex * 480}px)`,
         }}
       >
         {images.map((src, index) => (
@@ -38,9 +35,9 @@ const Carousel: React.FC = () => {
             src={src}
             alt={`Slide ${index + 1}`}
             style={{
-              width: '390px',
-              height: '332px',
-              objectFit: 'cover',
+              height: '408px',
+              width: '100%', // 부모 요소에 맞춤
+              // objectFit: 'cover',
               borderRadius: '0px 0px 20px 20px',
             }}
           />
@@ -49,7 +46,7 @@ const Carousel: React.FC = () => {
       <div
         style={{
           position: 'absolute',
-          bottom: '10px',
+          bottom: '20px',
           left: '50%',
           transform: 'translateX(-50%)',
           width: '238px',
@@ -71,7 +68,6 @@ const Carousel: React.FC = () => {
       <button onClick={handlePrev} css={LeftBtn}>
         ‹
       </button>
-
       <button onClick={handleNext} css={RightBtn}>
         ›
       </button>
