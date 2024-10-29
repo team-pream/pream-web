309f40d388fbed5e045f039003be365f6f0f2ae9
minju0423 <125239229+minju0423@users.noreply.github.com>
2024-10-29 19:21:34 +0900

    [PREAM-24] 상품 목록 페이지에서 상품 리스트를 조회할 수 있다. (#56)
    * feat: 상품 목록 페이지 UI 구현 (#40)
    
    * [PREAM-34] 카테고리에 해당하는 상품 조회 (#42)
    
    * feat: 상품 목록 페이지 UI 구현 (#40)
    
    * feat: 카테고리에 해당하는 상품 조회
    
    * feat: 상태별 상품 목록 필터링
    
    * fix: 상태 텍스트 변경
    
    * refactor: 코드 리팩토링
    
    * fix: 코드리뷰사항 수정
    
    * fix: 코드리뷰사항 수정
    
    * fix: 상품 제목 변경
    
    * fix: GetProductsParamType로 변경
    
    * fix: 예약중(reserved) 필터 추가

--- src/queries/query-keys.ts
+++ src/queries/query-keys.ts
@@ -4,4 +4,5 @@ export const QUERY_KEYS = {
   GET_CATEGORIES: ['GET_CATEGORIES'],
   GET_PRODUCTS: (params: GetProductsParamType) => ['GET_PRODUCTS', params],
   GET_CURATION: ['GET_CURATION'],
+  GET_PRODUCTS: (params: GetProductsParamType) => ['GET_PRODUCTS', params],
 } as const;
