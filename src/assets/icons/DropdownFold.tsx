74ad7d0d108191ff99725b54d6f36b1412689bbb
sieon <sieon.eve@gmail.com>
2024-10-29 10:02:29 +0900

    fix: 최종 결제 금액 컴포넌트 생성

--- src/assets/icons/DropdownFold.tsx
+++ src/assets/icons/DropdownFold.tsx
@@ -2,7 +2,7 @@ import type { SVGProps } from 'react';
 const SvgDropdownFold = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6" {...props}>
     <path
-      stroke="#000"
+      stroke={props.stroke || '#000'}
       strokeLinecap="round"
       strokeLinejoin="round"
       strokeMiterlimit={10}
