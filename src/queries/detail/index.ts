6721f7302d3d2e87b2e53c1a1d500937a0f96c0e
sieon <sieon.eve@gmail.com>
2024-10-29 14:40:17 +0900

    feat: 카드 및 다른 결제수단 중 하나만 선택

--- src/queries/detail/index.ts
+++ src/queries/detail/index.ts
@@ -1,14 +1,13 @@
-// hooks/useGetProductQuery.ts
 import { useQuery } from '@tanstack/react-query';
 import { getProduct, Product } from '@/api/detail';
 import { QUERY_KEYS } from '../query-keys';
 
-export const useGetProductQuery = (productId: string) => {
+export const useGetProductQuery = (productId: number) => {
   return useQuery<Product, Error>({
     queryKey: QUERY_KEYS.GET_PRODUCT(productId),
-    queryFn: async () => {
-      if (!productId) throw new Error('상품 ID가 제공되지 않았습니다.');
-      return await getProduct(productId);
-    },
+    queryFn: () => getProduct(productId),
+    enabled: !!productId, // productId가 유효할 때만 실행
+    staleTime: 0, // 항상 최신 데이터 사용
+    refetchOnWindowFocus: false, // 창 활성화 시 재요청 방지
   });
 };
