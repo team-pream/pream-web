3b2661f90103077dabc9c8b06010a19731f9ca8a
sieon <sieon.eve@gmail.com>
2024-10-29 10:02:29 +0900

    fix: 최종 결제 금액 컴포넌트 생성

--- src/pages/detail/index.styles.ts
+++ src/pages/detail/index.styles.ts
@@ -4,8 +4,8 @@ import theme from '@/styles/theme';
 export const Wrapper = css`
   /* width: 480px; */
   max-width: 480px;
-  height: 100vh;
-  min-height: 950px;
+  height: 100%;
+  min-height: 844px;
   background-color: white;
   display: flex;
   flex-direction: column;
@@ -24,7 +24,7 @@ export const Header = css`
 
 export const Contents = css`
   min-height: 710px;
-  overflow-x: hidden; // 캐러셀 이미지 숨기기
+  /* overflow-x: hidden; // 캐러셀 이미지 숨기기 */
   height: 100%;
   padding-bottom: 78px;
   -ms-overflow-style: none; /* IE와 Edge에서 스크롤바 숨기기 */
