0a980cf615b5c6ab37afa8d9456361ad02682b0d
corinthionia <corinthionia@users.noreply.github.com>
2024-10-30 00:49:07 +0000

    deploy: c307bfe84a56e0956ca6f99609edf8373d71dd1d

---
+++ assets/index.stories-QgGDDHf8.js
@@ -0,0 +1,34 @@
+import{j as C}from"./theme-BvZHPoXC.js";import{I as E,B as I}from"./index-BwdbtUkM.js";import"./extends-CKwpSh1k.js";import"./index-CTjT7uj6.js";const _={title:"Components/Input",component:E,tags:["autodocs"]},e={args:{label:"판매 희망가",placeholder:"판매 희망가를 입력해 주세요."}},r={args:{label:"판매 희망가",autoFocus:!0}},s={args:{label:"판매 희망가",errorMessage:"최소 판매 가격은 100원입니다."}},a={args:{label:"판매 희망가",confirmMessage:"판매 희망가가 확인되었습니다."}},o={args:{label:"판매 희망가",confirmMessage:"판매 희망가가 확인되었습니다.",suffix:C(I,{variant:"box",size:"xs",children:"확인"})}},t={args:{label:"판매 희망가",disabled:!0}};var n,c,l;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    label: '판매 희망가',
+    placeholder: '판매 희망가를 입력해 주세요.'
+  }
+}`,...(l=(c=e.parameters)==null?void 0:c.docs)==null?void 0:l.source}}};var u,m,i;r.parameters={...r.parameters,docs:{...(u=r.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    label: '판매 희망가',
+    autoFocus: true
+  }
+}`,...(i=(m=r.parameters)==null?void 0:m.docs)==null?void 0:i.source}}};var p,d,g;s.parameters={...s.parameters,docs:{...(p=s.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    label: '판매 희망가',
+    errorMessage: '최소 판매 가격은 100원입니다.'
+  }
+}`,...(g=(d=s.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};var f,b,x;a.parameters={...a.parameters,docs:{...(f=a.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    label: '판매 희망가',
+    confirmMessage: '판매 희망가가 확인되었습니다.'
+  }
+}`,...(x=(b=a.parameters)==null?void 0:b.docs)==null?void 0:x.source}}};var S,M,h;o.parameters={...o.parameters,docs:{...(S=o.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    label: '판매 희망가',
+    confirmMessage: '판매 희망가가 확인되었습니다.',
+    suffix: <Button variant="box" size="xs">
+        확인
+      </Button>
+  }
+}`,...(h=(M=o.parameters)==null?void 0:M.docs)==null?void 0:h.source}}};var B,D,F;t.parameters={...t.parameters,docs:{...(B=t.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  args: {
+    label: '판매 희망가',
+    disabled: true
+  }
+}`,...(F=(D=t.parameters)==null?void 0:D.docs)==null?void 0:F.source}}};const O=["Default","Focused","Error","Confirmed","WithSuffix","Disabled"];export{a as Confirmed,e as Default,t as Disabled,s as Error,r as Focused,o as WithSuffix,O as __namedExportsOrder,_ as default};
