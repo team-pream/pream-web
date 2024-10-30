2e53dcbb7b910188bce41b8e8b9a9e0a199eec8d
Yoonjeong Ko <jejukyj@naver.com>
2024-10-30 17:26:57 +0900

    fix: 충돌 해결

--- src/pages/category/index.tsx
+++ src/pages/category/index.tsx
@@ -3,14 +3,7 @@ import { gridWrapper, menuWrapper, hr } from './index.styles';
 import { AppBar, GNB, Layout, SearchBar, Text } from '@/components';
 import { AppBarBack } from '@/assets/icons';
 import CategoryItem from './components/category-item';
-<<<<<<< HEAD
 import theme from '@/styles/theme';
-=======
-import { gridWrapper, wrapper, menuWrapper, hr } from './index.styles';
-import { AppBar, GNB, SearchBar, Text } from '@/components';
-import theme from '@/styles/theme';
-import { AppBarBack } from '@/assets/icons';
->>>>>>> fbff0a4 ([PREAM-108] 공통 UI 컴포넌트 적용 (#38))
 
 interface CategoryType {
   id: number;
@@ -24,11 +17,7 @@ export default function Category() {
   const { data } = useGetCategoriesQuery();
 
   return (
-<<<<<<< HEAD
     <Layout>
-=======
-    <div css={wrapper}>
->>>>>>> fbff0a4 ([PREAM-108] 공통 UI 컴포넌트 적용 (#38))
       <AppBar prefix={<AppBarBack height="24px" cursor="pointer" />} suffix={<SearchBar />} />
 
       <div css={menuWrapper}>
@@ -46,10 +35,6 @@ export default function Category() {
       </div>
 
       <GNB />
-<<<<<<< HEAD
     </Layout>
-=======
-    </div>
->>>>>>> fbff0a4 ([PREAM-108] 공통 UI 컴포넌트 적용 (#38))
   );
 }
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,10 +1,6 @@
 import { wrapper, kakaoButton, loginText, subtitle } from './index.styles';
 import { KakaoLogo, Logo } from '@/assets/icons';
-<<<<<<< HEAD
 import { Layout, Text } from '@/components';
-=======
-import { Text } from '@/components';
->>>>>>> fbff0a4 ([PREAM-108] 공통 UI 컴포넌트 적용 (#38))
 import theme from '@/styles/theme';
 
 export default function Login() {
@@ -13,7 +9,6 @@ export default function Login() {
   };
 
   return (
-<<<<<<< HEAD
     <Layout>
       <div css={wrapper}>
         <Logo width="172px" />
@@ -34,25 +29,5 @@ export default function Login() {
         </Text>
       </div>
     </Layout>
-=======
-    <div css={wrapper}>
-      <Logo width="172px" />
-
-      <Text typo="body2" color={theme.colors.black} css={subtitle}>
-        당신과 댕냥이를 위한 합리적인 소비
-      </Text>
-
-      <button css={kakaoButton} onClick={handleKakaoLoginButtonClick}>
-        <KakaoLogo width="20px" />
-        <Text typo="body1" color={theme.colors.black}>
-          카카오톡으로 시작하기
-        </Text>
-      </button>
-
-      <Text typo="body6" color={theme.colors.gray300}>
-        이미 계정이 있으신가요? <span css={loginText}>로그인하기</span>
-      </Text>
-    </div>
->>>>>>> fbff0a4 ([PREAM-108] 공통 UI 컴포넌트 적용 (#38))
   );
 }
