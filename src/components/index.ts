02485bcfdf9daad8299408656eeb3c15ac50caf2
Yoonjeong Ko <jejukyj@naver.com>
2024-10-30 17:26:57 +0900

    fix: 충돌 해결

--- src/components/index.ts
+++ src/components/index.ts
@@ -9,10 +9,7 @@ export * from './dim';
 export * from './fab';
 export * from './gnb';
 export * from './input';
-<<<<<<< HEAD
 export * from './layout';
-=======
->>>>>>> fbff0a4 ([PREAM-108] 공통 UI 컴포넌트 적용 (#38))
 export * from './radio-group';
 export * from './search-bar';
 export * from './text';
