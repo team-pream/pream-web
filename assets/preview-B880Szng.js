ca3a2bbe6706659c8f37ec541ab482380f23641a
jejukyj <jejukyj@users.noreply.github.com>
2024-10-31 02:23:17 +0000

    deploy: b145f4e6c7ecc777fb7caa65aafa04392a49c47a

--- assets/preview-B880Szng.js
+++ assets/preview-B880Szng.js
@@ -12,6 +12,7 @@ import{j as o}from"./extends-CKwpSh1k.js";import{r,a as y}from"./index-CTjT7uj6.
     font-family: 'Pretendard Variable', 'Pretendard', '-apple-system', 'BlinkMacSystemFont',
       'system-ui', 'Roboto', 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR',
       'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'sans-serif';
+
     width: 100%;
     height: 100%;
     color: ${i.colors.black100};
