1bd9a593f9d536d663714667f2427136db3c66f2
minju0423 <125239229+minju0423@users.noreply.github.com>
2024-11-01 10:23:12 +0900

    [PREAM-124] 반려동물 프로필 편집 화면 개발 (#81)
    * feat: 프로필 편집 페이지 UI 구현
    
    * fix: 파일 경로 변경
    
    * design: 버튼 추가
    
    * fix: 사용하지 않는 css 제거
    
    * fix: query key 수정
    
    * fix: 로그아웃 후 메인페이지로 이동
    
    * design: 디자인 수정
    
    * fix: 반려동물 정보수정 페이지 수정
    
    * fix: 반려동물 정보 없을 때 UI수정

--- src/pages/mypage/pages/pet-profile/index.tsx
+++ src/pages/mypage/pages/pet-profile/index.tsx
@@ -1,3 +1,130 @@
+import { AppBarBack, MypageUploadImage } from '@/assets/icons';
+import { AppBar, Button, Dialog, Input, Layout, RadioGroup, Text } from '@/components';
+import { useNavigate } from 'react-router-dom';
+import {
+  buttonWrapper,
+  DeleteButton,
+  fixedButtonWrapper,
+  formStyle,
+  hr,
+  imageIcon,
+  profileEditWrapper,
+  profileImageWrapper,
+  radioStyle,
+  uploadIcon,
+  uploadIconWrapper,
+} from './index.styles';
+import { useState } from 'react';
+import theme from '@/styles/theme';
+
 export default function PetProfile() {
-  return <></>;
+  const navigate = useNavigate();
+  const [selected, setSelected] = useState<string>('DOG');
+  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
+  const data = {
+    name: null,
+  }; //추후 api 연동 후 삭제
+
+  const PET_TYPE_OPTION = [
+    {
+      value: 'DOG',
+      node: (
+        <Button variant="capsule" status={selected === 'DOG' ? 'active' : 'disabled'}>
+          강아지
+        </Button>
+      ),
+    },
+    {
+      value: 'CAT',
+      node: (
+        <Button variant="capsule" status={selected === 'CAT' ? 'active' : 'disabled'}>
+          고양이
+        </Button>
+      ),
+    },
+  ];
+
+  const openDialog = () => {
+    setIsDialogOpen(true);
+  };
+  const closeDialog = () => {
+    setIsDialogOpen(false);
+  };
+  return (
+    <Layout>
+      <AppBar
+        prefix={
+          <AppBarBack
+            height="17px"
+            cursor="pointer"
+            onClick={() => {
+              navigate(-1);
+            }}
+          />
+        }
+      />
+      <div css={profileEditWrapper}>
+        <div>
+          {!data || !data?.name ? (
+            <Text typo="title1">댕냥이 등록</Text>
+          ) : (
+            <Text typo="title1">댕냥이 정보 수정</Text>
+          )}
+          <hr css={hr} />
+        </div>
+        <div css={profileImageWrapper}>
+          <img src="/images/petprofile.png" alt="profile" css={imageIcon} />
+          <div css={uploadIconWrapper}>
+            <MypageUploadImage css={uploadIcon} />
+          </div>
+        </div>
+        <div css={radioStyle}>
+          <RadioGroup
+            name="petType"
+            options={PET_TYPE_OPTION}
+            selectedValue="DOG"
+            onChange={(value: string) => {
+              setSelected(value);
+            }}
+            style={{ gap: '12px' }}
+          ></RadioGroup>
+        </div>
+        <div css={formStyle}>
+          <Input label="이름" placeholder="반려동물의 이름을 입력해주세요"></Input>
+        </div>
+      </div>
+      <div css={buttonWrapper}>
+        {!data || !data?.name ? (
+          <>
+            <div css={fixedButtonWrapper}>
+              <Button size="xl">등록하기</Button>
+            </div>
+          </>
+        ) : (
+          <>
+            <div css={fixedButtonWrapper}>
+              <Button size="xl">수정하기</Button>
+            </div>
+            <div onClick={openDialog} css={DeleteButton}>
+              <Text typo="subtitle2" color={theme.colors.gray300}>
+                삭제하기
+              </Text>
+              {isDialogOpen && (
+                <Dialog
+                  title="프로필 삭제"
+                  description="프로필을 삭제하시겠습니까?"
+                  primaryActionLabel="Save"
+                  secondaryActionLabel="Cancle"
+                  onPrimaryAction={() => {
+                    console.log('click');
+                  }}
+                  onSecondaryAction={closeDialog}
+                />
+              )}
+            </div>
+          </>
+        )}
+      </div>
+    </Layout>
+  );
 }
