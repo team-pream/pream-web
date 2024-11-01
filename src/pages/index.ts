ad8301a764411fc5b355e6d7f02c65673460904b
sieon <sieon.eve@gmail.com>
2024-11-01 21:57:25 +0900

    fix: pnpm-lock 수정

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -6,7 +6,4 @@ export { default as MyPage } from './mypage';
 export { default as Onboarding } from './onboarding';
 export { default as Orders } from './orders';
 export { default as Products } from './products';
-<<<<<<< HEAD
 export { default as NotFound } from './not-found';
-=======
->>>>>>> 4fb9e9b (chore: page/address 파일 mypage안으로 이전)
