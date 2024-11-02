73184eb76c0af5168aefdd338c96cbfca2d23598
Yoonjeong Ko <jejukyj@naver.com>
2024-11-03 00:35:42 +0900

    [PREAM-22] 메인페이지 FAB 및 배너이미지 추가 (#97)
    * [PREAM-27] 메인페이지 섹션별 상품 조회 UI 구현 (#54)
    
    feat: 메인페이지 섹션별 상품 조회 UI 구현
    
    * [PREAM-103] 메인페이지 섹션별 상품 조회 기능 구현 (#63)
    
    * [PREAM-87] 메인페이지 카테고리 UI 구현 및 라우터 연결 (#8)
    
    * feat: 메인페이지 카테고리 UI 구현 및 라우터 연결
    
    * refactor: react-query를 사용하여 api 호출 및 리팩토링
    
    * refactor: 코드리뷰를 반영해서 리팩토링
    
    * refactor: 코드리뷰를 반영해서 리팩토링
    
    * fix: 충돌 해결
    
    * feat: 메인페이지 공통 컴포넌트 적용 및 섹션별 상품조회 기능 구현
    
    * refactor: 상품별 라우터 연결 및 코드 리팩토링
    
    * [PREAM-24] 상품 목록 페이지에서 상품 리스트를 조회할 수 있다. (#56)
    
    * feat: 상품 목록 페이지 UI 구현 (#40)
    
    * [PREAM-34] 카테고리에 해당하는 상품 조회 (#42)
    
    * feat: 상품 목록 페이지 UI 구현 (#40)
    
    * feat: 카테고리에 해당하는 상품 조회
    
    * feat: 상태별 상품 목록 필터링
    
    * fix: 상태 텍스트 변경
    
    * refactor: 코드 리팩토링
    
    * fix: 코드리뷰사항 수정
    
    * fix: 코드리뷰사항 수정
    
    * fix: 상품 제목 변경
    
    * fix: GetProductsParamType로 변경
    
    * fix: 예약중(reserved) 필터 추가
    
    * [PREAM-87] 메인페이지 카테고리 UI 구현 및 라우터 연결 (#8)
    
    * feat: 메인페이지 카테고리 UI 구현 및 라우터 연결
    
    * refactor: react-query를 사용하여 api 호출 및 리팩토링
    
    * refactor: 코드리뷰를 반영해서 리팩토링
    
    * refactor: 코드리뷰를 반영해서 리팩토링
    
    * feat: 메인페이지 공통 컴포넌트 적용 및 섹션별 상품조회 기능 구현
    
    * refactor: 공통 컴포넌트 적용 및 코드 리팩토링
    
    * fix: 충돌 해결
    
    * refactor: 리팩토링
    
    * refactor: 코드 리뷰를 반영하여 리팩토링
    
    ---------
    
    Co-authored-by: minju0423 <125239229+minju0423@users.noreply.github.com>
    
    * refactor: sampleImage 제거
    
    * design: 배너 이미지 추가
    
    * refactor: 카테고리 navigate 경로 수정 및 FAB 추가
    
    * refactor: 코드 리뷰를 반영하여 리팩토링
    
    ---------
    
    Co-authored-by: minju0423 <125239229+minju0423@users.noreply.github.com>

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -8,8 +8,8 @@ import {
 import { useNavigate } from 'react-router-dom';
 import { useGetCategoriesQuery } from '@/queries/categories';
 import { useGetProductsCurationQuery } from '@/queries/products';
-import { Logo, Search } from '@/assets/icons';
-import { AppBar, GNB, Layout, Text, Carousel } from '@/components';
+import { FabPlus, Logo, Search } from '@/assets/icons';
+import { AppBar, GNB, Layout, Text, Carousel, FAB } from '@/components';
 import ProductList from './components/product-list';
 import { Category } from '@/types';
 import theme from '@/styles/theme';
@@ -34,12 +34,13 @@ export default function Main() {
   // 배너 이미지
   const imageList = [
     'images/carouselImage.png',
-    'images/carouselImage.png',
-    'images/carouselImage.png',
+    'images/carouselImage2.png',
+    'images/carouselImage3.png',
   ];
 
   return (
     <Layout>
+      <FAB icon={<FabPlus width="20" height="20" />} onClick={() => navigate('/products/upload')} />
       <AppBar
         prefix={<Logo width="94" height="20" />}
         suffix={
@@ -75,7 +76,7 @@ export default function Main() {
                           navigate(`/products?category=${category.id}`, {
                             state: { id: category.id, name: category.name },
                           })
-                      : () => navigate('/category')
+                      : () => navigate('/categories')
                   }
                 >
                   <img src={category.icon} alt={category.name} css={categoryIcon} />
