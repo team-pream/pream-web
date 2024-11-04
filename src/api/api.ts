b35c31d7c16c6a44d64a17aed11f30be23fb1934
Yoonjeong Ko <jejukyj@naver.com>
2024-11-04 11:50:44 +0900

    refactor: 코드 리뷰를 반영하여 리팩토링

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,7 +4,6 @@ export const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
-    Authorization: `Bearer ${localStorage.getItem('access')}`,
   },
 });
 
