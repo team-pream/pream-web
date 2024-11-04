220e56ee35078f5d1880be56e4e1798fe4e96ef7
minju0423 <lmj042398@gmail.com>
2024-11-04 10:14:25 +0900

    feat: 카테고리 페이지 검색기능 구현

--- src/pages/categories/index.tsx
+++ src/pages/categories/index.tsx
@@ -5,15 +5,41 @@ import { AppBarBack } from '@/assets/icons';
 import CategoryItem from './components/category-item';
 import theme from '@/styles/theme';
 import { Category } from '@/types';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const ALL_MENU_ID: number = 1; //전체 메뉴에 대한 id
 
 export default function Categories() {
   const { data } = useGetCategoriesQuery();
+  const navigate = useNavigate();
+  const [keyword, setKeyword] = useState<string>('');
 
+  const handleChangeKeyword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(event.target.value);
+  };
+
+  // Enter 키 이벤트 핸들러
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      event.preventDefault(); // 기본 동작 방지
+      navigate(`/products/search?keyword=${keyword}`, { state: { keyword } });
+    }
+  };
   return (
     <Layout>
-      <AppBar prefix={<AppBarBack height="24px" cursor="pointer" />} suffix={<SearchBar />} />
+      <AppBar
+        prefix={
+          <AppBarBack
+            height="24px"
+            cursor="pointer"
+            onClick={() => {
+              navigate(-1);
+            }}
+          />
+        }
+        suffix={<SearchBar onChange={handleChangeKeyword} onKeyDown={handleKeyDown} />}
+      />
 
       <div css={menuWrapper}>
         <Text typo="title1" color={theme.colors.black}>
