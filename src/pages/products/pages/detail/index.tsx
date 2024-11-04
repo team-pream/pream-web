4586362f813a7975ea3adc1dd332fbf83affeac0
Yoonjeong Ko <jejukyj@naver.com>
2024-11-04 16:12:52 +0900

    refactor: 테스트 코드 제거

--- src/pages/products/pages/detail/index.tsx
+++ src/pages/products/pages/detail/index.tsx
@@ -57,10 +57,6 @@ export default function Detail() {
       />
     );
 
-  console.log('product: ', product);
-  console.log('isFetching: ', isFetching);
-  console.log('isSuccess: ', isSuccess);
-
   if (!product && !isFetching)
     return (
       <Dialog
