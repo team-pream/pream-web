ee836935ca27516303115995a81a40b0c9389f23
minju0423 <minju0423@users.noreply.github.com>
2024-11-05 00:37:23 +0000

    deploy: d697f84655cafbfd3dadaaf44c084bddbbfcc8f9

---
+++ assets/index.stories-BBqiFdlF.js
@@ -0,0 +1,6 @@
+import{t as a}from"./theme-BvZHPoXC.js";import"./index-BZbfpbvN.js";import"./index-CTjT7uj6.js";import{T as s}from"./index-D5nyNoYp.js";import"./extends-CKwpSh1k.js";import"./index-BZqGpNeu.js";const d={title:"Components/Text",component:s,tags:["autodocs"],argTypes:{typo:{control:"select",options:Object.keys(a)}}},t={args:{typo:"title1",children:"프림 Pream"}};var e,o,r;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    typo: 'title1',
+    children: '프림 Pream'
+  }
+}`,...(r=(o=t.parameters)==null?void 0:o.docs)==null?void 0:r.source}}};const u=["Default"];export{t as Default,u as __namedExportsOrder,d as default};
--- assets/index.stories-BbY0zsXp.js
+++
@@ -1,3 +0,0 @@
-import{b as e}from"./index-AMLnIp10.js";import"./index-CTjT7uj6.js";import"./index-D5nyNoYp.js";import"./theme-BvZHPoXC.js";import"./extends-CKwpSh1k.js";import"./index-BZqGpNeu.js";const d={title:"Components/SearchBar",tags:["autodocs"],component:e},r={args:{}};var o,t,a;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {}
-}`,...(a=(t=r.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,d as default};
