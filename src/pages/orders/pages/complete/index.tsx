2bdb0b9770be558cb12707e7026854bfc2174021
minju0423 <125239229+minju0423@users.noreply.github.com>
2024-11-06 19:06:45 +0900

    [PREAM-107] 구매 완료 페이지에서 결제가 성공적으로 이루어졌음을 확인할 수 있다. (#134)
    * [PREAM-141] 구매완료 화면 구현 (#95)
    
    * feat: 구매완료 페이지 UI 구현
    
    * fix: 코드리뷰사항 반영
    
    * [PREAM-163] 결제 실패 페이지 화면 구현 (#122)
    
    * feat: 결제 실패 아이콘 추가
    
    * feat: 결제실패 페이지 개발
    
    * fix: 구매완료 페이지 수정

--- src/pages/orders/pages/complete/index.tsx
+++ src/pages/orders/pages/complete/index.tsx
@@ -1,3 +1,92 @@
-export default function Complete() {
-  return <></>;
+import { AppBarBack, Complete } from '@/assets/icons';
+import { AppBar, Button, Layout, Text } from '@/components';
+import {
+  blockTitle,
+  completeIcon,
+  confirmWrapper,
+  hr,
+  info,
+  infoContent,
+  infoTitle,
+  InnerWrapper,
+  orderInfoWrapper,
+  payInfo,
+  paymentBlock,
+  productImg,
+  productInfoWrapper,
+  shipBlock,
+  textWrapperStyle,
+  wrapper,
+} from './index.styles';
+import { useNavigate } from 'react-router-dom';
+
+export default function OrderComplete() {
+  const navigate = useNavigate();
+  return (
+    <Layout>
+      <AppBar prefix={<AppBarBack height="24px" cursor="pointer" />} />
+      <div css={wrapper}>
+        <div css={confirmWrapper}>
+          <Complete css={completeIcon} />
+          <Text typo="title1">구매 완료!</Text>
+          <Button
+            shape="box"
+            size="s"
+            onClick={() => {
+              navigate('/');
+            }}
+          >
+            메인으로
+          </Button>
+        </div>
+        <hr css={hr} />
+        <div css={orderInfoWrapper}>
+          <div css={productInfoWrapper}>
+            <Text typo="subtitle1">주문 상품 정보</Text>
+            <div css={InnerWrapper}>
+              <div css={productImg}>
+                <img alt="상품사진" />
+              </div>
+              <div css={textWrapperStyle}>
+                <Text typo="body4">판매명</Text>
+                <Text typo="subtitle1">22,800원</Text>
+              </div>
+            </div>
+          </div>
+          <div css={shipBlock}>
+            <Text typo="subtitle1" css={blockTitle}>
+              배송 정보
+            </Text>
+            <div css={info}>
+              <div css={infoTitle}>
+                <Text typo="subtitle2">받는 분</Text>
+                <Text typo="subtitle2">주소</Text>
+                <Text typo="subtitle2">휴대폰번호</Text>
+              </div>
+              <div css={infoContent}>
+                <Text typo="body4">고윤정</Text>
+                <Text typo="body4">서울 강남구 선릉로 428 멀티캠퍼스</Text>
+                <Text typo="body4">010-0000-0000</Text>
+              </div>
+            </div>
+          </div>
+          <div css={paymentBlock}>
+            <Text typo="subtitle1" css={blockTitle}>
+              결제 정보
+            </Text>
+            <div css={payInfo}>
+              <div css={infoTitle}>
+                <Text typo="subtitle2">결제 방법</Text>
+                <Text typo="subtitle2">결제 금액</Text>
+              </div>
+              <div css={infoContent}>
+                <Text typo="body4">토스페이</Text>
+                <Text typo="body4">20,000원</Text>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
---
+++ src/pages/orders/pages/failure/index.styles.ts
@@ -0,0 +1,19 @@
+import { css } from '@emotion/react';
+
+export const failureWrapper = css`
+  width: 100%;
+  height: 90vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const text = css`
+  margin: 35px 0 16px 0;
+`;
+
+export const failureIcon = css`
+  width: 70px;
+  height: 70px;
+`;
