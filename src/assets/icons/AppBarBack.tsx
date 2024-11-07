75df11cde7f21cc80a61ffc06297129195994922
Joohyun Kim <corinthionia@gmail.com>
2024-11-07 23:20:03 +0900

    fix: update icon

---
+++ src/assets/icons/AppBarBack.tsx
@@ -0,0 +1,10 @@
+import type { SVGProps } from 'react';
+const SvgAppBarBack = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 9 20" {...props}>
+    <path
+      fill="#000"
+      d="M7.39 16.995a.58.58 0 0 1-.434-.198L1.18 10.47a.71.71 0 0 1 0-.946l5.776-6.327a.572.572 0 0 1 .864 0c.24.263.24.684 0 .946L2.474 10l5.346 5.856c.24.263.24.684 0 .947a.6.6 0 0 1-.434.198z"
+    />
+  </svg>
+);
+export default SvgAppBarBack;
