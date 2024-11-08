d2bc7ea0f42c4bfcf45e51050d583d11a56fd0a2
sieon <sieon.eve@gmail.com>
2024-11-08 09:42:55 +0900

    fix: 토스트 메시지 확인

--- src/pages/mypage/pages/address/index.tsx
+++ src/pages/mypage/pages/address/index.tsx
@@ -1,14 +1,15 @@
 import { AppBar, Layout } from '@/components';
-import { AppBarBack, FabPlus } from '@/assets/icons';
+import { AppBarBack, Clear, FabPlus } from '@/assets/icons';
 import { GNB, FAB } from '@/components';
 import AddressList from './components/address-list';
 import { useState, useEffect } from 'react';
 import AddressForm from './components/address-form';
 import { useGetUsersProfileQuery } from '@/queries';
 import { PatchUsersAddressBody } from '@/types';
-import { useNavigate } from 'react-router-dom';
-import { Dialog } from '@/components';
-
+import { useNavigate, useLocation } from 'react-router-dom';
+import { Dialog, Text } from '@/components';
+import theme from '@/styles/theme';
+import { editClear } from '../main/index.styles';
 type PageState = 'list' | 'form';
 
 interface AddressData {
@@ -22,19 +23,43 @@ export default function Address() {
   const { data, refetch } = useGetUsersProfileQuery(); // refetch 추가
   const [userData, setUserData] = useState<AddressData | null>(null);
   const [isCancelDialogOpen, setIsCancelDialogOpen] = useState<boolean>(false); // 추가
+
+  const location = useLocation();
+  const [showUpdateMessage, setShowUpdateMessage] = useState<string | false>(
+    location.state?.editSuccess || false
+  );
   const navigate = useNavigate();
 
   useEffect(() => {
+    if (showUpdateMessage) {
+      const timer = setTimeout(() => {
+        setShowUpdateMessage(false);
+      }, 2000);
+
+      return () => clearTimeout(timer);
+    }
     if (data) {
       const { username, phone, address } = data;
       if (address) setUserData({ username, phone, address });
       else setUserData(null);
     }
-  }, [data]);
+  }, [data, showUpdateMessage]);
+
+  const getMessage = () => {
+    switch (showUpdateMessage) {
+      case 'registered':
+        return '주소가 등록되었습니다.';
+      case 'updated':
+        return '주소가 수정되었습니다.';
+      default:
+        return '';
+    }
+  };
 
   const handleSaveAddress = () => {
     refetch();
     setPage('list');
+    setShowUpdateMessage('registered'); // 등록 메시지 표시
   };
 
   const handleBackClick = () => {
@@ -54,6 +79,14 @@ export default function Address() {
           <AddressList userData={userData} onEditClick={() => userData && setPage('form')} />
           {!userData && (
             <FAB icon={<FabPlus width="20px" height="20px" />} onClick={() => setPage('form')} />
+          )}
+          {showUpdateMessage && (
+            <div css={editClear}>
+              <Clear width="16px" style={{ marginRight: '8px' }} />
+              <Text typo="body4" color={theme.colors.white}>
+                {getMessage()}
+              </Text>
+            </div>
           )}
         </>
       ) : (
