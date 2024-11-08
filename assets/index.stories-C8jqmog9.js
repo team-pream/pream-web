9046c0ac6f0937f1dc5515ce742aa668e3148402
corinthionia <corinthionia@users.noreply.github.com>
2024-11-08 03:34:14 +0000

    deploy: 9500d1699730978662264e603e127e04f2553ef2

--- assets/index.stories-C8jqmog9.js
+++
@@ -1,3 +0,0 @@
-import{a as e}from"./index-bRBvw1Tc.js";import"./index-CTjT7uj6.js";import"./index-D5nyNoYp.js";import"./theme-BvZHPoXC.js";import"./extends-CKwpSh1k.js";import"./index-BZqGpNeu.js";const d={title:"Components/SearchBar",tags:["autodocs"],component:e},r={args:{}};var a,o,t;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {}
-}`,...(t=(o=r.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,d as default};
---
+++ assets/index.stories-Tvlvu_Lg.js
@@ -0,0 +1,21 @@
+import{D as d}from"./index-CTmIaUOS.js";import"./index-DRjF_FHU.js";import"./index-kAFjU4do.js";import"./theme-DPbiucBH.js";import"./jsx-runtime-DR9Q75dM.js";import"./index-BKbKZLLv.js";const b={title:"Components/Dialog",component:d,tags:["autodocs"]},r={args:{title:"Primary Dialog",description:"Primary Dialog Description",primaryActionLabel:"Action"}},o={args:{title:"Secondary Dialog",description:"Secondary Dialog Description",primaryActionLabel:"Action",secondaryActionLabel:"Cancel"}},a={args:{type:"error",title:"제목",description:"에러에 대한 설명",primaryActionLabel:"메인으로 돌아가기"}};var e,i,t;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    title: 'Primary Dialog',
+    description: 'Primary Dialog Description',
+    primaryActionLabel: 'Action'
+  }
+}`,...(t=(i=r.parameters)==null?void 0:i.docs)==null?void 0:t.source}}};var n,c,s;o.parameters={...o.parameters,docs:{...(n=o.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    title: 'Secondary Dialog',
+    description: 'Secondary Dialog Description',
+    primaryActionLabel: 'Action',
+    secondaryActionLabel: 'Cancel'
+  }
+}`,...(s=(c=o.parameters)==null?void 0:c.docs)==null?void 0:s.source}}};var p,m,l;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    type: 'error',
+    title: '제목',
+    description: '에러에 대한 설명',
+    primaryActionLabel: '메인으로 돌아가기'
+  }
+}`,...(l=(m=a.parameters)==null?void 0:m.docs)==null?void 0:l.source}}};const L=["Primary","Secondary","Error"];export{a as Error,r as Primary,o as Secondary,L as __namedExportsOrder,b as default};
