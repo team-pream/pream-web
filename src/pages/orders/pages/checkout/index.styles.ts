bb84a7898d994babde3547fba81b5a3db902441c
minju0423 <125239229+minju0423@users.noreply.github.com>
2024-11-08 15:18:21 +0900

    [PREAM-181] 에러사항 수정 (#185)
    * fix: 에러 사항 수정
    
    * fix: 문구수정

--- src/pages/orders/pages/checkout/index.styles.ts
+++ src/pages/orders/pages/checkout/index.styles.ts
@@ -13,3 +13,27 @@ export const title = css`
 export const line = css`
   border-bottom: 2px solid ${colors.black};
 `;
+
+export const wrapper = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 90vh;
+  width: 100%;
+  margin: 0px auto;
+  gap: 30px;
+`;
+
+export const iconWrapper = css`
+  width: 85px;
+  height: 74px;
+`;
+
+export const textWrapper = css`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  justify-content: center;
+  align-items: center;
+`;
--- src/pages/orders/pages/checkout/index.tsx
+++ src/pages/orders/pages/checkout/index.tsx
@@ -1,9 +1,12 @@
 import { useParams, useNavigate } from 'react-router-dom';
 import { AppBarBack } from '@/assets/icons';
-import { Layout, AppBar, Text } from '@/components';
+import { Layout, AppBar, Text, Button } from '@/components';
 import { useGetProductsDetailQuery, useGetUsersProfileQuery } from '@/queries';
 import { OrderForm } from './components/order-form';
-import { title, line, contentsWrapper } from './index.styles';
+import { title, line, contentsWrapper, wrapper, iconWrapper, textWrapper } from './index.styles';
+import SvgNotFound from '@/assets/icons/NotFound';
+import { ROUTE_PATHS } from '@/constants/routes';
+import theme from '@/styles/theme';
 
 export default function Order() {
   const navigate = useNavigate();
@@ -12,8 +15,29 @@ export default function Order() {
   const { data: user } = useGetUsersProfileQuery();
   const { data: product } = useGetProductsDetailQuery(productId!);
 
-  if (!product || !user) return alert('로그인이 필요해요.');
-
+  if (!product || !user) {
+    return (
+      <Layout>
+        <div css={wrapper}>
+          <SvgNotFound css={iconWrapper} />
+          <div css={textWrapper}>
+            <Text typo="title1">로그인이 필요해요</Text>
+            <Text typo="body4" color={theme.colors.gray300}>
+              로그인을 해주세요
+            </Text>
+          </div>
+          <Button
+            size="s"
+            onClick={() => {
+              navigate(ROUTE_PATHS.LOGIN);
+            }}
+          >
+            로그인
+          </Button>
+        </div>
+      </Layout>
+    );
+  }
   return (
     <Layout>
       <AppBar prefix={<AppBarBack height="24px" cursor="pointer" onClick={() => navigate(-1)} />} />
