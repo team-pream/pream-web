82600daa0925494f65e34fa2ec85c408d31b34e1
minju0423 <minju0423@users.noreply.github.com>
2024-11-08 05:11:05 +0000

    deploy: 158b569d0ce9024cda0c052e6b5ae00b30f9223a

---
+++ assets/index.stories-Bd531u_E.js
@@ -0,0 +1,17 @@
+import{j as r}from"./theme-BvZHPoXC.js";import{A as B,S,a as l,b as A,c as k}from"./index-bRBvw1Tc.js";import"./index-CTjT7uj6.js";import"./index-D5nyNoYp.js";import"./extends-CKwpSh1k.js";import"./index-BZqGpNeu.js";const L={title:"Components/AppBar",component:B},e={args:{}},a={args:{prefix:r(S,{height:"24px",cursor:"pointer"})}},s={args:{prefix:r(S,{height:"24px",cursor:"pointer"}),suffix:r(l,{autoFocus:!0})}},o={args:{prefix:r(A,{width:"94px",height:"24px"}),suffix:r(k,{width:"20px",height:"20px",cursor:"pointer"})}};var p,t,c;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {}
+}`,...(c=(t=e.parameters)==null?void 0:t.docs)==null?void 0:c.source}}};var i,n,u;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    prefix: <AppBarBack height="24px" cursor="pointer" />
+  }
+}`,...(u=(n=a.parameters)==null?void 0:n.docs)==null?void 0:u.source}}};var h,m,g;s.parameters={...s.parameters,docs:{...(h=s.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    prefix: <AppBarBack height="24px" cursor="pointer" />,
+    suffix: <SearchBar autoFocus />
+  }
+}`,...(g=(m=s.parameters)==null?void 0:m.docs)==null?void 0:g.source}}};var x,d,f;o.parameters={...o.parameters,docs:{...(x=o.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    prefix: <Logo width="94px" height="24px" />,
+    suffix: <SearchIcon width="20px" height="20px" cursor="pointer" />
+  }
+}`,...(f=(d=o.parameters)==null?void 0:d.docs)==null?void 0:f.source}}};const _=["Default","Back","SearchInput","Search"];export{a as Back,e as Default,o as Search,s as SearchInput,_ as __namedExportsOrder,L as default};
